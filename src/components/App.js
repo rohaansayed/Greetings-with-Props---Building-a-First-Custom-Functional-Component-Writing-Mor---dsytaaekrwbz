4c6bc7a513c303c9d8375a31f06961510db664c9
rohaansayed <119560435+rohaansayed@users.noreply.github.com>
2023-02-18 08:41:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,9 @@ import '../styles/App.css';
 import Welcome from './Welcome';
 
 const App = () => {
+  let name = "Rohaan sayed";
   return (
-    <Welcome name="" />
+    <Welcome name={name} />
   )
 }
 
